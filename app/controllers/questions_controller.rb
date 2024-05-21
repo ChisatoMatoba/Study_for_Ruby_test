6ab24ea1be829b12464a721a8e71221e6f190234
Chisato Matoba <chisato.matoba@luxiar.com>
2024-05-21 22:26:45 +0900

    上書き修正機能

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,9 @@
 class QuestionsController < ApplicationController
   def import
-    @category = Category.find(params[:category_id])
+    @category = Category.find_by(id: params[:category_id])
     if params[:file].present?
-      Question.import(params[:file], @category)
+      overwrite = params[:overwrite] == '1'
+      Question.import(params[:file], @category, overwrite)
       redirect_to category_questions_path(@category), notice: '正常にインポートできました'
     else
       redirect_to new_category_question_path(@category), alert: 'インポートに失敗しました'
--- app/models/question.rb
+++ app/models/question.rb
@@ -10,18 +10,36 @@ class Question < ApplicationRecord
     validates :number, uniqueness: { scope: :category_id }
   end
 
-  def self.import(file, category)
+  def self.import(file, category, overwrite)
     CSV.foreach(file.path, headers: true) do |row|
-      question = category.questions.create!(
-        number: row['number'],
-        content: row['question_content'],
-        explanation: row['explanation']
-      )
+      question = category.questions.find_by(number: row['number'])
 
-      row['choices'].split(',').each do |choice|
-        content, is_correct = choice.split('|')
-        question.choices.create!(content: content.strip, is_correct: is_correct.strip == 'true')
+      if question
+        if overwrite
+          question.update!(
+            content: row['question_content'],
+            explanation: row['explanation']
+          )
+          question.choices.destroy_all
+          choices_create(question, row['choices'])
+        end
+      else
+        category.questions.create!(
+          number: row['number'],
+          content: row['question_content'],
+          explanation: row['explanation']
+        )
+        choices_create(question, row['choices'])
       end
+    end
+  end
+
+  private
+
+  def self.choices_create(question, choices)
+    choices.split(',').each do |choice|
+      content, is_correct = choice.split('|')
+      question.choices.create!(content: content.strip, is_correct: is_correct.strip == 'true')
     end
   end
 end
