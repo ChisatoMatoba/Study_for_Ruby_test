b24c3e5d434d36bf96c0dc57d7c9c2936cea49cd
Chisato Matoba <chisato.matoba@luxiar.com>
2024-05-27 21:06:41 +0900

    ログイン認証

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+
   def import
     @category = Category.find_by(id: params[:category_id])
     if params[:file].present?
@@ -12,5 +14,25 @@ class QuestionsController < ApplicationController
     else
       redirect_to category_questions_path(@category), alert: 'インポートに失敗しました'
     end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def answer
+    @question = Question.find(params[:id])
+    selected_choices = params[:choices] || []
+    correct_choices = question.choices.where(is_correct: true).pluck(:id).map(&:to_s)
+    is_correct = (selected_choices.sort == correct_choices.sort)
+
+    current_user.quiz_results.create!(
+      category: @question.category,
+      question: @question,
+      correct: is_correct,
+      answer_detail: selected_choices.join(',')
+    )
+
+    # 次の質問へのリダイレクトや結果表示の処理を
   end
 end
