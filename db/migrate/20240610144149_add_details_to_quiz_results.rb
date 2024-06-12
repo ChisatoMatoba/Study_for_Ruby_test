7baeecfc6ba2cb1603917fe6824447f1c6c21136
Chisato Matoba <chisato.matoba@luxiar.com>
2024-06-12 21:05:38 +0900

    db修正

--- db/migrate/20240610144149_add_details_to_quiz_results.rb
+++ db/migrate/20240610144149_add_details_to_quiz_results.rb
@@ -3,6 +3,6 @@ class AddDetailsToQuizResults < ActiveRecord::Migration[7.1]
     add_column :quiz_results, :selected, :json, null: false
     add_column :quiz_results, :correct, :json, null: false
     add_column :quiz_results, :is_correct, :boolean, null: false
-    add_column :quiz_results, :session_ts, :datetime, null: false
+    add_column :quiz_results, :session_ts, :bigint, null: false
   end
 end
