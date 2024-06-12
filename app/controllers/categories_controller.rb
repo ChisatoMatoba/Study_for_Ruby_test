0243cadaf0daf3ce82e63437a47893671f65e79f
Chisato Matoba <chisato.matoba@luxiar.com>
2024-06-12 21:36:43 +0900

    結果

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -47,6 +47,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @questions = @category.questions
 
+    session_ts = Time.current.strftime('%Y%m%d%H%M%S').to_i
     @quiz_results = []
     session[:results].each do |question_id, result|
       quiz_result = QuizResult.create!(
@@ -55,10 +56,17 @@ class CategoriesController < ApplicationController
         question_id: question_id,
         selected: result['selected'],
         correct: result['correct'],
-        is_correct: result['is_correct']
+        is_correct: result['is_correct'],
+        session_ts: session_ts
       )
       @quiz_results << quiz_result
     end
+  end
+
+  def results_by_session
+    @session_ts = params[:session_ts].to_i
+    @quiz_results = QuizResult.where(session_ts: @session_ts)
+    @category = Category.includes(:questions).find(@quiz_results.first.category_id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
       get :results # 結果表示アクション
     end
 
+    collection do
+      get 'results_by_session/:session_ts', to: 'categories#results_by_session', as: 'results_by_session'
+    end
+
     resources :questions do
       member do
         post :check_answer # 回答チェックアクション
