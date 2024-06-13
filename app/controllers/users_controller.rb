4e3d16df25abade16e10d9ec370bbaa1d892dd48
Chisato Matoba <chisato.matoba@luxiar.com>
2024-06-13 22:40:58 +0900

    補足

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # セッションタイムスタンプで結果を集計する
     @quiz_results = QuizResult.aggregate_results_by_session
   end
 end
