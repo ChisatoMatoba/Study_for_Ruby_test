77ea8b1e2979ff7f696bff221f947808c443dd31
Chisato Matoba <chisato.matoba@luxiar.com>
2024-06-13 22:39:26 +0900

    quiz_result保存のリファクタ

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,19 +3,7 @@ class ResultsController < ApplicationController
 
   def create_quiz_results
     session_ts = Time.current.strftime('%Y%m%d%H%M%S').to_i
-    @quiz_results = []
-    session[:results].each do |question_id, result|
-      quiz_result = QuizResult.create!(
-        user_id: current_user.id,
-        category_id: Category.find(Question.find(question_id).category_id).id,
-        question_id: question_id,
-        selected: result['selected'],
-        correct: result['correct'],
-        is_correct: result['is_correct'],
-        session_ts: session_ts
-      )
-      @quiz_results << quiz_result
-    end
+    @quiz_results = save_quiz_results(session[:results], session_ts)
 
     redirect_to results_path(session_ts: @quiz_results.first.session_ts)
   end
@@ -41,5 +29,24 @@ class ResultsController < ApplicationController
     else
       redirect_to user_path(current_user), alert: '削除するデータが見つかりませんでした。'
     end
+  end
+
+  private
+
+  def save_quiz_results(results, session_ts)
+    quiz_results = []
+    results.each do |question_id, result|
+      quiz_result = QuizResult.create!(
+        user_id: current_user.id,
+        category_id: Category.find(Question.find(question_id).category_id).id,
+        question_id: question_id,
+        selected: result['selected'],
+        correct: result['correct'],
+        is_correct: result['is_correct'],
+        session_ts: session_ts
+      )
+      quiz_results << quiz_result
+    end
+    quiz_results
   end
 end
