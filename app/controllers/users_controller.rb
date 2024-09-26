fcc077400e7d8b8d15c5c6b887f8c8c0542eb05f
Chisato Matoba <chisato.matoba@luxiar.com>
2024-09-27 07:39:32 +0900

    メモをマイページで見られるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # セッションタイムスタンプで結果を集計する
     @quiz_results = QuizResult.aggregate_results_by_session
+
+    # 現在のユーザーが保存したすべてのメモを取得
+    @questions_with_memos = Question.where.not(memo: [nil, ''])
   end
 end
