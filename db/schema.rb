c81989f5bb7f50fdf538d50a3f5a03debb901ce1
Chisato Matoba <chisato.matoba@luxiar.com>
2024-10-04 20:26:01 +0900

    session保存スキーマ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_22_075047) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_30_025528) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -51,6 +51,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_22_075047) do
     t.index ["category_id"], name: "index_quiz_results_on_category_id"
     t.index ["question_id"], name: "index_quiz_results_on_question_id"
     t.index ["user_id"], name: "index_quiz_results_on_user_id"
+  end
+
+  create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
