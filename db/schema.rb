78e4b4a42fac2a618e28f27cba4be366bd91ce6c
Chisato Matoba <chisato.matoba@luxiar.com>
2025-01-02 17:06:20 +0900

    メモカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_02_075310) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_02_080439) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -46,7 +46,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_02_075310) do
     t.text "explanation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "memo"
     t.index ["category_id", "number"], name: "index_questions_on_category_id_and_number", unique: true
     t.index ["category_id"], name: "index_questions_on_category_id"
   end
