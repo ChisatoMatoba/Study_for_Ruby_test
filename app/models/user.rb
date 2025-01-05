5c43222f7ac7553676d30b69f31d8397ee5bab8e
Chisato Matoba <chisato.matoba@luxiar.com>
2025-01-05 17:36:37 +0900

    enum設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :name,                presence: true
   validates :email,               presence: true
   validates :encrypted_password,  presence: true
+  validates :role,                presence: true
+
+  enum role: { general: 0, admin: 1, owner: 2 }
 end
