ee9d24adc6a6c4cef0c74308022035e8bafbe55d
Chisato Matoba <chisato.matoba@luxiar.com>
2025-03-08 18:28:10 +0900

    一意性確保

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :question do
     category
-    number { Faker::Number.number(digits: 2) }
+    number { SecureRandom.random_number(10_000) }
     content { Faker::Lorem.sentence }
     explanation { Faker::Lorem.sentence }
   end
